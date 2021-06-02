b25c97e646de705db1efdea6286c1611fe0d46f5
Emerson3851 <71472550+Emerson3851@users.noreply.github.com>
2021-06-01 23:46:50 -0300

    Add files via upload

---
+++ Atividade/Curriculo/script.js
@@ -0,0 +1,73 @@
+    
+    function apertar1(){
+
+    document.getElementById("seta1").style.display ='block';
+    document.getElementById("seta2").style.display ='none';
+    document.getElementById("seta3").style.display ='none';
+    document.getElementById("seta4").style.display ='none';
+    document.getElementById("seta5").style.display ='none';
+    document.getElementById("seta6").style.display ='none';
+
+ }
+
+    function apertar2(){
+
+    document.getElementById("seta1").style.display ='none';
+    document.getElementById("seta2").style.display ='block';
+    document.getElementById("seta3").style.display ='none';
+    document.getElementById("seta4").style.display ='none';
+    document.getElementById("seta5").style.display ='none';
+    document.getElementById("seta6").style.display ='none';
+    
+        
+ }
+
+    
+    function apertar3(){
+
+    document.getElementById("seta1").style.display ='none';
+    document.getElementById("seta2").style.display ='none';
+    document.getElementById("seta3").style.display ='block';
+    document.getElementById("seta4").style.display ='none';
+    document.getElementById("seta5").style.display ='none';
+    document.getElementById("seta6").style.display ='none';
+    
+
+}
+
+    function apertar4(){
+
+    document.getElementById("seta1").style.display ='none';
+    document.getElementById("seta2").style.display ='none';
+    document.getElementById("seta3").style.display ='none';
+    document.getElementById("seta4").style.display ='block';
+    document.getElementById("seta5").style.display ='none';
+    document.getElementById("seta6").style.display ='none';
+
+}
+
+    function apertar5(){
+
+    document.getElementById("seta1").style.display ='none';
+    document.getElementById("seta2").style.display ='none';
+    document.getElementById("seta3").style.display ='none';
+    document.getElementById("seta4").style.display ='none';
+    document.getElementById("seta5").style.display ='block';
+    document.getElementById("seta6").style.display ='none';
+
+}
+    
+    function apertar6(){
+
+    document.getElementById("seta1").style.display ='none';
+    document.getElementById("seta2").style.display ='none';
+    document.getElementById("seta3").style.display ='none';
+    document.getElementById("seta4").style.display ='none';
+    document.getElementById("seta5").style.display ='none';
+    document.getElementById("seta6").style.display ='block';
+   
+}
+
+    
+        
+  
